2f8bf3ad6dd4f1de86aca707fd5a3580db74ea89
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-05-11 19:17:37 +0200

    Center posts and add space for text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,7 @@
 import LostJudgment from '../../public/LostJudgment.jpg'
+import Mirage from '../../public/Mirage.jpg'
+import Aloy from '../../public/Aloy.jpg'
+import Aleksios from '../../public/Aleksios.jpg'
 
 
 function Main() {
@@ -6,16 +9,19 @@ function Main() {
         <div className="main">
             <div className="post">
                 <img src={LostJudgment} alt='Lost Judgment Menu Photo'></img>
-                <p>lost jugment</p>
+                <p>Lost Judgment</p>
             </div>
             <div className="post">
-                <img src={LostJudgment} alt='Lost Judgment Menu Photo'></img>
+                <img src={Mirage} alt='Mirage Logo Photo'></img>
+                <p>Mirage</p>
             </div>
             <div className="post">
-                <img src={LostJudgment} alt='Lost Judgment Menu Photo'></img>
+                <img src={Aloy} alt='Aloy Photo'></img>
+                <p>Aloy</p>
             </div>
             <div className="post">
-                <img src={LostJudgment} alt='Lost Judgment Menu Photo'></img>
+                <img src={Aleksios} alt='Aleksios Photo'></img>
+                <p>Aleksios</p>
             </div>
         </div>
     )
