a03c9ce6237f3f92d07f2f0ec4b38a75bf78d5d8
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-05-13 12:05:11 +0200

    Css properties for main section and thumbails for posts

--- src/Pages/ThumbailPosts.jsx
+++ src/Pages/ThumbailPosts.jsx
@@ -1,26 +1,27 @@
 import Odyssey from '/Oddysey.jpeg';
 import Judgment from '/Judgment.jpg';
-
+import Aloy from '/Aloy.jpg';
+import Blacklist from '/Blacklist.png';
 
 
 function Posts() {
   return (
-    <div className="news_container">
+    <div className="thumbail_container">
       <article className="article">
             <img className="news_image" src={Odyssey} alt='Aleksios on tree photo'></img>
             <a href="Co lubię w Assasin's Creed Odyssey" className='news_text'>Co lubię w Assassins Creed Odyssey ?</a>
           </article>
         <article className="article">
             <img className="news_image" src={Judgment} alt='Yagami photo'></img>
-            <a href="" className='news_text'>Jak zaczęła się moja przygoda z grami z serii Yakuza ?</a>
+            <a href="" className='news_text'>Jak zaczęła się moja przygoda z grami z serii Yakuza?</a>
         </article>
         <article className="article">
-            <img className="news_image" src={Odyssey} alt='Aleksios on tree photo'></img>
-            <a href="Co lubię w Assasin's Creed Odyssey" className='news_text'>Co lubię w Assassins Creed Odyssey ?</a>
+            <img className="news_image" src={Aloy} alt='Aloyphoto'></img>
+            <a href="Co lubię w Assasin's Creed Odyssey" className='news_text'>Przemierzając świat z Aloy</a>
         </article>
         <article className="article">
-            <img className="news_image" src={Judgment} alt='Yagami photo'></img>
-            <a href="" className='news_text'>Jak zaczęła się moja przygoda z grami z serii Yakuza ?</a>
+            <img className="news_image" src={Blacklist} alt='Sam Fisher photo'></img>
+            <a href="" className='news_text'>Kiedy Sam Fisher powróci?</a>
           </article>
     </div>
   )
