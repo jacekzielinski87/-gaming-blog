577fb26210d06dfe104f2e24fda9a37df719a4fe
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-05-13 14:10:53 +0200

    Add content to main section and change thee css properties

--- src/Pages/ThumbailPosts.jsx
+++ src/Pages/ThumbailPosts.jsx
@@ -2,6 +2,8 @@ import Odyssey from '/Oddysey.jpeg';
 import Judgment from '/Judgment.jpg';
 import Aloy from '/Aloy.jpg';
 import Blacklist from '/Blacklist.png';
+import Leon from '/Leon.jpg';
+import Sekiro from '/Sekiro.jpg';
 
 
 function Posts() {
@@ -23,6 +25,15 @@ function Posts() {
             <img className="news_image" src={Blacklist} alt='Sam Fisher photo'></img>
             <a href="" className='news_text'>Kiedy Sam Fisher powróci?</a>
           </article>
+          <article className="article">
+            <img className="news_image" src={Leon} alt='Leon S kennedy photo'></img>
+            <a href="" className='news_text'>O Capcomie i Residencie słów kilka</a>
+          </article>
+          <article className="article">
+            <img className="news_image" src={Sekiro} alt='Wolf photo'></img>
+            <a href="" className='news_text'>Sekiro okiem świeżaka</a>
+          </article>
+          <p className='read_more'>Czytaj dalej</p>
     </div>
   )
 }
